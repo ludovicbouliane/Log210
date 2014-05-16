f12568bd94062a03f0279eaf95f697e4f27c08d6
Marc Grenier <mrcgrenier@gmail.com>
2014-05-16 10:32:17 -0400

    Connection for client page added

--- WEB/partial/header.php
+++ WEB/partial/header.php
@@ -4,8 +4,11 @@
 		<title><?php echo $titre; ?> </title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
 		<link href="/css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/global.css" rel="stylesheet" />
+		<link href="css/common.css" rel="stylesheet" />
+		<link href="css/global.css" rel="stylesheet">
+		
 		
 	</head>
 	<body>
