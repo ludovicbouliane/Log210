b745b1e58afe91c28035f539eeac30839988fda2
Marc Grenier <mrcgrenier@gmail.com>
2014-05-28 14:28:26 -0400

    moved a div from files to partial

--- WEB/addRestaurantManager.php
+++ WEB/addRestaurantManager.php
@@ -8,8 +8,6 @@
 
 	require_once('partial/site_header.php');
 ?>
-	<div class="col-sm-2"></div>
-
 	<div class="col-sm-8 content">
 		<h2> Ajouter un restaurateur</h2>
 
@@ -58,7 +56,6 @@
 		</div>
 		
 	</div>
-	<div class="col-sm-2"></div>
 <?php
 	require_once("partial/site_footer.php");
 ?>
