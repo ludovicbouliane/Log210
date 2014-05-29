6d56a7a167e08c92093fd8d116a8cd41ac8ed92a
marc-andrecroteau <marcandre.croteau@gmail.com>
2014-05-29 17:34:10 -0400

    Bug d'update de RestaurantManager

--- API/DataAccess/Repositories/RestaurantManagerRepository.cs
+++ API/DataAccess/Repositories/RestaurantManagerRepository.cs
@@ -5,5 +5,19 @@ namespace DataAccess.Repositories
 {
     public class RestaurantManagerRepository : Repository<RestaurantManager>, IRestaurantManagerRepository
     {
+        public override void Save(params RestaurantManager[] items)
+        {
+            foreach (var restaurantManager in items)
+            {
+                var collection = _database.GetCollection<RestaurantManager>(typeof(RestaurantManager).Name.ToLower() + "s");
+
+                var existingRestaurantManager = GetSingle(r => r.Id == restaurantManager.Id);
+
+                if (restaurantManager.FirstName != null) existingRestaurantManager.FirstName = restaurantManager.FirstName;
+                if (restaurantManager.LastName != null) existingRestaurantManager.LastName = restaurantManager.LastName;
+
+                collection.Save(existingRestaurantManager);
+            }
+        }
     }
 }
