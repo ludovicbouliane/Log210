26e9f2d28ad1d95a62ec199d9c08514bea187541
Marc Grenier <mrcgrenier@gmail.com>
2014-06-05 14:10:56 -0400

    added validation for restaurant

--- WEB/editRestaurant.php
+++ WEB/editRestaurant.php
@@ -10,12 +10,12 @@
 
 ?>
 
-	<div class="col-sm-8 content">
+	<div class="col-sm-8 content" id="editRestaurantForm">
 		<h2> Modifier un restaurant</h2>
 
 		<div id="message"></div>
 
-		<div class="row form_row">
+		<div class="row form_row form-group">
 			<div class="col-sm-4">Restaurant : </div>
 			<div class="col-sm-8">
 				<select id="listRestaurant" class="form-control" onchange="fillRestaurantInfos()"> 
@@ -23,52 +23,52 @@
 			</div>
 		</div>
 
-		<div class="row form_row">
+		<div class="row form_row form-group">
 			<div class="col-sm-4">Nom :</div>
 			<div class="col-sm-8">
-				<input type="text" class="form-control" id="name" placeholder="Nom" required/>
+				<input type="text" class="form-control" id="name" placeholder="Nom" name="name"/>
 			</div>
 		</div>
 
-		<div class="row form_row">
+		<div class="row form_row form-group">
 			<div class="col-sm-4">Adresse :</div>
 			<div class="col-sm-8">
-				<input type="text" class="form-control" id="address" placeholder="Adresse" required/>
+				<input type="text" class="form-control" id="address" placeholder="Adresse" name="address"/>
 			</div>
 		</div>
 
-		<div class="row form_row">
+		<div class="row form_row form-group">
 			<div class="col-sm-4">Ville :</div>
 			<div class="col-sm-8">
-				<input type="text" class="form-control" id="city" placeholder="Ville" required/>
+				<input type="text" class="form-control" id="city" placeholder="Ville" name="city"/>
 			</div>
 		</div>
 		
-		<div class="row form_row">
+		<div class="row form_row form-group">
 			<div class="col-sm-4">Province :</div>
 			<div class="col-sm-8">
-				<input type="text" class="form-control" id="state" placeholder="Province" required/>
+				<input type="text" class="form-control" id="state" placeholder="Province" name="state"/>
 			</div>
 		</div>
 		
-		<div class="row form_row">
+		<div class="row form_row form-group">
 			<div class="col-sm-4">Pays :</div>
 			<div class="col-sm-8">
-				<input type="text" class="form-control" id="country" placeholder="Pays" required/>
+				<input type="text" class="form-control" id="country" placeholder="Pays" name="country"/>
 			</div>
 		</div>
 		
-		<div class="row form_row">
+		<div class="row form_row form-group">
 			<div class="col-sm-4">Code postal :</div>
 			<div class="col-sm-8">
-				<input type="text" maxlength=7 class="form-control" id="zipCode" placeholder="Code postal" required/>
+				<input type="text" maxlength=7 class="form-control" id="zipCode" placeholder="Code postal" name="zipCode"/>
 			</div>
 		</div>
 		
-		<div class="row form_row">
+		<div class="row form_row form-group">
 			<div class="col-sm-4">Numéro de téléphone <p>(555 555-5555)</p></div>
 			<div class="col-sm-8">
-				<input type="text" maxlength=12 class="form-control" id="phoneNumber" placeholder="Numéro de téléphone" required/>
+				<input type="text" maxlength=12 class="form-control" id="phoneNumber" placeholder="Numéro de téléphone" name="phoneNumber"/>
 			</div>
 		</div>
 
@@ -82,6 +82,8 @@
 <?php
 	require_once('partial/site_footer.php');
 ?>
+
+<script type="text/javascript" src="/js/validator/editRestaurantValidator.js"></script>
 <script type="text/javascript">
 	window.onload = fillRestaurantList(getAllRestaurantByContractor());
 </script>
---
+++ WEB/js/validator/addRestaurantValidator.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+    $('#addRestaurantForm').bootstrapValidator({
+        message: 'This value is not valid',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            name:{
+                validators:{
+                    notEmpty:{
+                        message : 'Le nom est obligatoire'                        
+                    }
+                }
+            },
+            address:{
+                validators:{
+                    notEmpty:{
+                        message : 'L\'adresse est obligatoire'                        
+                    }
+                }
+            },
+            city:{
+                validators:{
+                    notEmpty:{
+                        message : 'La ville est obligatoire'                        
+                    }
+                }
+            },
+            state:{
+                validators:{
+                    notEmpty:{
+                        message : 'L\'état est obligatoire'                        
+                    }
+                }
+            },
+            country:{
+                validators:{
+                    notEmpty:{
+                        message : 'Le pays est obligatoire'                        
+                    }
+                }
+            },
+            zipCode:{
+                validators:{
+                    notEmpty:{
+                        message : 'Le code postal est obligatoire'                        
+                    },
+                    zipCode: {
+                        min: 6,
+                        max: 7,
+                        message: 'Le code postal doit avoir entre 6 et 7 charactères'
+                    }
+                }
+            },
+            phoneNumber:{
+                validators:{
+                    notEmpty:{
+                        message : 'Le numéro de téléphone est obligatoire'                        
+                    },
+                    regexp:{
+                        regexp: /(\d{3}) (\d{3})-(\d{4})/,
+                        message: 'Le numéro de téléphone ne respecte pas le format demandé'
+                    }
+                }
+            }
+        }
+    });
+});
