5c7c5a225660e0d9c46013de69ffb47a2efb2e74
marc-andrecroteau <marcandre.croteau@gmail.com>
2014-06-13 09:51:33 -0400

    Add gitignore
    Change RestaurantManagerWithAccount

--- API/Model/ControllerModel/RestaurantManagerWithAccount.cs
+++ API/Model/ControllerModel/RestaurantManagerWithAccount.cs
@@ -6,6 +6,7 @@ namespace Model.ControllerModel
     public class RestaurantManagerWithAccount
     {
         public Account Account { get; set; }
+        public string ContractorUsername { get; set; }
         public List<string> RestaurantIds { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
